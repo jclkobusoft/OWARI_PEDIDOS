573770f028f8e8ced9ec29a8cec4b54f0399e0ec
Javier Rodríguez <jargu@kobuosft.com>
2026-08-08 12:06:37 -0600

    fix (cuando $info_cliente viene null)
    el cliente no se encontró en el SAE / su dato CAMPLIB14 no existe

--- resources/views/emails/pedido_especial.blade.php
+++ resources/views/emails/pedido_especial.blade.php
@@ -14,7 +14,7 @@
 		</div>
 		<h2 style="color:#c62828;">Hay un nuevo pedido especial de SYD (proveedor S227).</h2>
 		<p>Este pedido debe surtirse con el proveedor cercano <b>Suspension Y Direccion (S227)</b> inmediatamente.</p>
-	@elseif($info_cliente['CAMPLIB14'] == "SI")
+	@elseif(($info_cliente['CAMPLIB14'] ?? '') == "SI")
 		<h2 style="color:red">Hay un nuevo pedido especial QUE SE DEBE COBRAR PRIMERO.</h2>
 	@else
 		<h2>Hay un nuevo pedido especial.</h2>
